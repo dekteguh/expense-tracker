048006f1a05e2bc83529b1837b1353b24d7c788d
Eko Teguh Widodo <dekteguh@gmail.com>
2017-06-18 14:01:15 +0700

    tambahan router post

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Expense Tracker' });
 });
 
+
+router.post('/', function (req, res) {
+  console.log(req);
+  let amount = parseInt(req.body.amount);
+  let description = req.body.description;
+});
+
 module.exports = router;
