6eb50f4855290b0967b3a60967ff72a32ef78c96
Eko Teguh Widodo <dekteguh@gmail.com>
2017-06-18 14:32:47 +0700

    Tambah class expense.js

---
+++ entities/expense.js
@@ -0,0 +1,8 @@
+const expense = class Expense {
+    constructor(amount, description) {
+        this.amount = amount;
+        this.description = description;
+    }
+}
+
+module.exports = expense;
