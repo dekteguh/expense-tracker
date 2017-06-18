dc25b6f24ed511d0416f703fafa6ffb1b085c90e
Eko Teguh Widodo <dekteguh@gmail.com>
2017-06-18 14:44:12 +0700

    Import expense.js di entities

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,17 @@
 var express = require('express');
 var router = express.Router();
+const Expense = require('../entities/expense'); // import expense
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Expense Tracker' });
 });
 
-
+/* POST form expense */
 router.post('/', function (req, res) {
-  console.log(req);
   let amount = parseInt(req.body.amount);
   let description = req.body.description;
+  
 });
 
 module.exports = router;
